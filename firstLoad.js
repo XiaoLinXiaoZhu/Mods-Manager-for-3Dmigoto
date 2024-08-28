df152448407d2d868caac84c504337c02daab477
XLXZ <2965969512@qq.com>
2024-08-28 18:50:26 +0800

    增加了第一次打开页面，以及多语言化。

---
+++ firstLoad.js
@@ -0,0 +1,163 @@
+const { ipcRenderer } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const { shell } = require("electron");
+
+const openFolder = (pathName)=>{
+	shell.openPath(path.join(pathName, "/"))
+}
+
+async function getRootDirFromSystemDialog() {
+    const rootdir = await ipcRenderer.invoke('get-rootdir');
+    //debug
+    console.log(rootdir);
+    return rootdir;
+}
+
+function translatePage(lang) {
+    //获取所有需要翻译的元素
+    const elements = document.querySelectorAll('[data-translate-key]');
+    //获取翻译文件
+    const translationPath = path.join(__dirname, 'locales', `${lang}.json`);
+    //读取翻译文件
+    const translation = JSON.parse(fs.readFileSync(translationPath));
+    //debug
+    if (translation) {
+        //翻译元素
+        elements.forEach((element) => {
+            const key = element.getAttribute('data-translate-key');
+            if (key in translation) {
+                element.textContent = translation[key];
+                //debug
+                console.log(`Translate ${key} to ${translation[key]}`);
+            }
+        });
+    }
+    else {
+        console.log('Translation file not found');
+    }
+}
+
+//-=======================页面加载完成后的事件=======================
+
+document.addEventListener('DOMContentLoaded', () => {
+    //默认切换为英文
+    translatePage('en');
+
+    //控制配置文件的创建
+    const configPath = path.join(__dirname, 'configs', 'user-config.json');
+
+    //配置属性
+    let userConfig = {
+        lang: localStorage.getItem('lang') || 'en',
+        rootdir: localStorage.getItem('rootdir') || '',
+        modLoaderDir: localStorage.getItem('modLoaderDir') || '',
+        modBackpackDir: localStorage.getItem('modBackpackDir') || ''
+    };
+
+    //依次打开 s-dialog 组件，s-dialog 组件会询问用户的默认设置
+    //如果用户点击了确认按钮，则保存用户的设置,并且切换到下一个 s-dialog 组件
+    //如果用户点击了取消按钮，则不保存用户的设置，并且切换到下一个 s-dialog 组件
+    //如果用户点击了关闭按钮，则不保存用户的设置，并且关闭所有的 s-dialog 组件
+    const sDialogs = document.querySelectorAll('.s-dialog');
+
+    //各个 s-dialog 组件的引用
+    const languagePicker = document.querySelector('#language-switcher');
+    const selectRootDir = document.querySelector('#select-rootdir');
+    const showModDir = document.querySelector('#show-moddir');
+
+    //debug
+    console.log(sDialogs);
+
+    
+
+
+    //延时1s打开第一个 s-dialog 组件
+    
+    //- 内部函数
+    const showDialog = (index) => {
+        if (index < sDialogs.length) {
+            sDialogs[index].show();
+            sDialogs[index].addEventListener('dismissed', () => {
+                showDialog(index + 1);
+                saveUserConfig();
+            });
+        }
+    }
+
+    function saveUserConfig() {
+        //保存用户的设置,保存在 localStorage 中
+        localStorage.setItem('rootdir', userConfig.rootdir);
+        localStorage.setItem('modLoaderDir', userConfig.modLoaderDir);
+        localStorage.setItem('modBackpackDir', userConfig.modBackpackDir);
+        localStorage.setItem('lang', userConfig.lang);
+
+        ipcRenderer.invoke('set-rootdir', userConfig.rootdir);
+    }
+
+
+    //- 监听事件
+    languagePicker.addEventListener('change', (e) => {
+        const langIndex = e.target.selectedIndex;
+        let lang = '';
+        //debug 
+        switch(langIndex) {
+            case 0:
+                console.log('lang: English');
+                lang = 'en';
+                break;
+            case 1:
+                console.log('lang: Chinese');
+                lang = 'zh-cn';
+                break;
+            default:
+                console.log('lang: Unknown');
+                lang = 'en';
+        }
+        translatePage(lang);
+        //ipcRenderer.invoke('save-user-config', { lang });
+    });
+
+    selectRootDir.addEventListener('click', async () => {
+        const exeDir = await getRootDirFromSystemDialog();
+        const rootdir = path.dirname(exeDir);
+        //让 select-rootdir 的 lable属性 为 用户选择的路径
+        if (exeDir !== '') {
+            selectRootDir.label = exeDir;
+
+            userConfig.rootdir = rootdir;
+            userConfig.modLoaderDir = exeDir;
+
+            ipcRenderer.invoke('set-rootdir', rootdir);
+            ipcRenderer.invoke('create-mod-resource-backpack');
+        }
+        else {
+            selectRootDir.label = 'Please select your 3dmigoto.exe file';
+            alert('Please select your 3dmigoto.exe file');
+        }
+        //debug
+        console.log(rootdir);
+        //ipcRenderer.invoke('save-user-config', { rootdir });
+    });
+
+    showModDir.addEventListener('click', async () => {
+        const rootdir = userConfig.rootdir;
+        if (fs.existsSync(rootdir)) {
+            const modBackpackDir = path.join(rootdir, 'modResourceBackpack');
+            showModDir.label = modBackpackDir;
+            userConfig.modBackpackDir = modBackpackDir;
+
+            openFolder(modBackpackDir);
+        }
+        console.log(modLoaderdir);
+        //ipcRenderer.invoke('save-user-config', { modLoaderdir });
+    });
+
+
+    //- 默认触发
+    setTimeout(() => {
+        showDialog(0);
+    }, 1000);
+}
+);
+
--- main.js
+++ main.js
@@ -1,15 +1,134 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
 // 实际上 ___dirname 是当前文件所在的目录,但是 最终的目标是要找到 modResourceBackpack 的根文件夹，所以要找到这个文件夹的路径
 // 通过 在第一次打开时询问 rootdir 来 确认文件保存的位置
 
+// 国际化，但是我不会使用i18next，这里就直接自定义自己方法来实现国际化
+// 通过传递一个json文件来实现国际化
+// 需要的时候，通过读取json文件来显示对应的语言的内容
+ipcMain.handle('get-translate', async (event, lang) => {
+  // 读取对应的json文件，文件位于locales文件夹下,文件名为lang.json
+  const langDir = path.join(__dirname, 'locales');
+  const langPath = path.join(langDir, `${lang}.json`);
+  if (fs.existsSync(langPath)) {
+    return JSON.parse(fs.readFileSync(langPath));
+  }
+  else {
+    return {};
+  }
+}
+);
+
+
+//首次打开时询问用户的默认设置的窗口
+ipcMain.handle('open-first-load-window', async (event) => {
+  //创建一个新的窗口
+  const firstLoadWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'firstLoad-preload.js'),
+      contextIsolation: false, // 启用 contextIsolation
+      nodeIntegration: true,
+    },
+  });
+  firstLoadWindow.loadFile('firstLoad.html');
+}
+);
+
+//创建 modResourceBackpack 文件夹，以及 presets 文件夹
+ipcMain.handle('create-mod-resource-backpack', async () => {
+  const modResourceDir = path.join(rootdir, 'modResourceBackpack');
+  const modsDir = path.join(rootdir, 'Mods');
+
+  
+  let modCount = 0;
+  if (fs.existsSync(modsDir)) {
+    fs.readdirSync(modsDir).forEach(file => {
+      modCount += 1;
+    }
+    );
+  }
+
+  let modResourceCount = 0;
+  if (fs.existsSync(modResourceDir)) {
+    fs.readdirSync(modResourceDir).forEach(file => {
+      modResourceCount += 1;
+    }
+    );
+  }
+
+  //总共分为四种情况：
+  //1. mods文件夹存在且modCount>0，modResourceBackpack文件夹存在且modResourceCount>0，将modResourceBackpack文件夹改名为modResourceBackpack.bak，然后创建modResourceBackpack文件夹，将mods文件夹里面的文件夹复制到 modResourceBackpack 文件夹
+  //2. mods文件夹存在且modCount>0，modResourceBackpack文件夹不存在或者modResourceCount = 0，创建modResourceBackpack文件夹，将mods文件夹里面的文件夹复制到 modResourceBackpack 文件夹
+  //3. mods文件夹存在但modCount = 0，modResourceBackpack文件夹存在且modResourceCount>0，将modResourceBackpack文件夹改名为modResourceBackpack.bak，然后创建modResourceBackpack文件夹
+  //4. mods文件夹存在但modCount = 0，modResourceBackpack文件夹不存在或者modResourceCount = 0，创建modResourceBackpack文件夹
+  //如果mods文件夹里面有文件夹，则将mods文件夹里面的文件夹复制到 modResourceBackpack 文件夹
+
+  if (fs.existsSync(modsDir) && modCount > 0) {
+    if (fs.existsSync(modResourceDir) && modResourceCount > 0) {
+      //将 modResourceBackpack 文件夹重命名为 modResourceBackpack.bak
+      fs.renameSync(modResourceDir, path.join(rootdir, 'modResourceBackpack.bak'));
+      //创建 modResourceBackpack 文件夹
+      fs.mkdirSync(modResourceDir);
+    }
+    else {
+      //创建 modResourceBackpack 文件夹
+      fs.mkdirSync(modResourceDir);
+    }
+    //将mods文件夹里面的文件夹复制到 modResourceBackpack 文件夹
+    fs.readdirSync(modsDir).forEach(file => {
+      fs.cpSync(path.join(modsDir, file), path.join(modResourceDir, file), { recursive: true });
+    }
+    );
+  }
+  else {
+    if (fs.existsSync(modsDir) && modCount === 0) {
+      if (fs.existsSync(modResourceDir) && modResourceCount > 0) {
+        //将 modResourceBackpack 文件夹重命名为 modResourceBackpack.bak
+        fs.renameSync(modResourceDir, path.join(rootdir, 'modResourceBackpack.bak'));
+        //创建 modResourceBackpack 文件夹
+        fs.mkdirSync(modResourceDir);
+      }
+      else {
+        //创建 modResourceBackpack 文件夹
+        fs.mkdirSync(modResourceDir);
+      }
+    }
+  }
+  
+  //创建 presets 文件夹
+  const presetsDir = path.join(rootdir, 'presets');
+  if (!fs.existsSync(presetsDir)) {
+    fs.mkdirSync(presetsDir);
+  }
+  else {
+    //将 presets 文件夹重命名为 presets.bak
+    fs.renameSync(presetsDir, path.join(rootdir, 'presets.bak'));
+    //创建 presets 文件夹
+    fs.mkdirSync(presetsDir);
+  }
+}
+);
+
+
+//保存用户的设置
+ipcMain.handle('save-user-config', async (event, userConfig) => {
+  //保存用户的设置
+  const configDir = path.join(__dirname, 'configs');
+  if (!fs.existsSync(configDir)) {
+    fs.mkdirSync(configDir);
+  }
+  fs.writeFileSync(path.join(configDir, 'user-config.json'), JSON.stringify(userConfig));
+}
+);
+
 // 先尝试从localStorage中获取rootdir，如果没有则设置为默认值__dirname
 let rootdir = ''
 
-
-ipcMain.handle('check-rootdir', async (event,dir) => {
+ipcMain.handle('check-rootdir', async (event, dir) => {
   //检查 dir 是否存在,如果存在则返回 true
   return fs.existsSync(dir);
 }
@@ -18,6 +137,23 @@ ipcMain.handle('check-rootdir', async (event,dir) => {
 ipcMain.handle('set-rootdir', async (event, dir) => {
   console.log("set rootdir: " + dir);
   rootdir = dir;
+}
+);
+
+ipcMain.handle('get-rootdir', async (event) => {
+  //提示选择 3dmigoto.exe 文件
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      { name: '3dmigoto', extensions: ['exe'] }
+    ]
+  });
+  if (!result.canceled) {
+    const exePath = result.filePaths[0];
+    //rootdir = path.dirname(exePath);
+    return exePath;
+  }
+  return '';
 }
 );
 
@@ -34,7 +170,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(rootdir, 'renderer.js'),
+      preload: path.join(__dirname, 'renderer-preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
     },
@@ -59,7 +195,7 @@ app.on('activate', () => {
 
 ipcMain.handle('get-mods', async () => {
   //增加纠错
-  if (rootdir === ''){
+  if (rootdir === '') {
     console.log("rootdir is empty");
     rootdir = __dirname;
   }
@@ -86,7 +222,7 @@ ipcMain.handle('get-mod-info', async (event, mod) => {
 });
 
 ipcMain.handle('apply-mods', async (event, mods) => {
-  const modsDir = path.join(rootdir, '3dmigoto','Mods');
+  const modsDir = path.join(rootdir, 'Mods');
   const modResourceDir = path.join(rootdir, 'modResourceBackpack');
 
   // 删除未选中的mod
@@ -138,6 +274,6 @@ ipcMain.handle('delete-preset', async (event, presetName) => {
   if (fs.existsSync(presetPath)) {
     fs.rmSync(presetPath);
     //debug
-    
+
   }
 });
